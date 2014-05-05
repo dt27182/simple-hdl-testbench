2abf65541f43d18d80f9d3d2d059cd6a1691b1a5
Wenyu Tang <wenyu@a7.Millennium.Berkeley.EDU>
2014-05-05 13:19:04 -0700

    add script to run dc on multiple designs

---
+++ vlsi/build/dc-syn/run-dc.sh
@@ -0,0 +1,27 @@
+for i in `seq 1 4`;
+do
+  for j in `seq 1 4`;
+  do
+    for k in `seq 1 1`;
+    do
+      scp wenyu@s141.millennium.berkeley.edu:~/multithread-transform/simple-hdl-testbench/generated/Cpu$i$j$k.v ~/multithread-transform/simple-hdl-testbench/vlsi/build/generated-src/Cpu.v
+      make
+      cp -R current-dc/reports reports$i$j$k
+      echo $i $j $k
+    done
+  done
+done
+
+for i in `seq 1 4`;
+do
+  for j in `seq $i 4`;
+  do
+    for k in `seq 0 0`;
+    do
+      scp wenyu@s141.millennium.berkeley.edu:~/multithread-transform/simple-hdl-testbench/generated/Cpu$i$j$k.v ~/multithread-transform/simple-hdl-testbench/vlsi/build/generated-src/Cpu.v
+      make
+      cp -R current-dc/reports reports$i$j$k
+      echo $i $j $k
+    done
+  done
+done
